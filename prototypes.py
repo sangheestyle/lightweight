ddb0075b6e971b526a3b33d40cfca3bc0ed9fc8c
Sanghee Kim <sangheestyle@gmail.com>
2016-04-03 23:06:33 -0400

    File/Folder Scaffolding from chapter 3

---
+++ prototypes.py
@@ -0,0 +1,30 @@
+import sys
+
+from django.conf import settings
+
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='b0mqvak1p2sqm6p#+8o8fyxf+ox(le)8&jh_5^sxa!=7!+wxj0',
+    ROOT_URLCONF='sitebuilder.urls',
+    MIDDLEWARE_CLASSES=(),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+        'sitebuilder',
+    ),
+    TEMPLATES=(
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+        },
+    ),
+    STATIC_URL='/static/',
+)
+
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    
+    execute_from_command_line(sys.argv)
+
